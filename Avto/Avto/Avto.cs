a17dfde4d07c7e4fa730f016f4b23de6dd8f99c4
batgutz <124032379+batgutz@users.noreply.github.com>
2026-02-05 08:26:41 +0100

    Add files via upload

---
+++ Avto/Avto/Avto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avto
+{
+    internal class Avto
+    {
+        int maxhitrost;
+        double avgporaba;
+        double kolicinagoriva;
+        int stprevozenih;
+        string regst;
+
+        public Avto(string r,int n,double p) //nov avto
+        {
+            maxhitrost = n;
+            avgporaba = p;
+            regst = r;
+            stprevozenih = 0;
+            kolicinagoriva = 0;
+        }
+        public Avto(string r,int n, double p,double k,int s)
+        {
+            maxhitrost = n;
+            avgporaba = p;
+            regst = r;
+            stprevozenih = s;
+            kolicinagoriva = k;
+        }
+
+        public void Izpis()
+        {
+            Console.WriteLine("-------- AVTO --------");
+            Console.WriteLine("registerska stevilka:   "+regst);
+            Console.WriteLine("maksimalna hitrost:   " + maxhitrost);
+            Console.WriteLine("povprecna poraba:   " + avgporaba);
+            Console.WriteLine("stevilo prevozenih km:   " + stprevozenih);
+            Console.WriteLine("kolicina goriva:   " + kolicinagoriva);
+            Console.WriteLine("----------------------");
+        }
+
+        public double Doseg()
+        {
+            return kolicinagoriva *100/avgporaba;
+        }
+        public double NaslednjiServis(int n)
+        {
+            return n - stprevozenih % n;
+
+        }
+    }
+
+}
---
+++ Avto/Avto/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Avto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Avto a1 = new Avto("GO-GF-915",165,6.8);
+            a1.Izpis();
+
+
+            Avto a2 = new Avto("LJ-B3-785", 200, 7.2,28.9,87345);
+            a2.Izpis();
+            Console.WriteLine("lahko prevozite se "+a2.Doseg()+" km");
+            Console.WriteLine("naslednji servis "+a2.NaslednjiServis(15000));
+        }
+    }
+}

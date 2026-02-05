3fdf868230905dccd882b30a448c7367026c82b9
batgutz <124032379+batgutz@users.noreply.github.com>
2026-02-05 08:56:07 +0100

    Add files via upload

---
+++ Oseba/Oseba.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Oseba
+{
+    internal class Oseba
+    {
+
+        string ime;
+        int starost;
+
+        public Oseba(string i, int s) //nov avto
+        {
+            ime = i;
+            starost = s;
+        }
+
+        public int GetAgeinMonths()
+        {
+            return starost*12;
+        }
+
+        public String IsAdult(Oseba x)
+        {
+
+            if (this.starost > 17 && this.ime == x.ime)
+            {
+                return "DA";
+            }
+            else
+            {
+                return "NE";
+            }
+        }
+        //public bool IsAdult(Oseba x)
+        //{
+            
+        //    if (this.starost >17 && this.ime == x.ime)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
+    }
+}
---
+++ Oseba/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Oseba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Oseba o1 = new Oseba("Lynx", 12);
+            Console.WriteLine("starost v mesecih : "+o1.GetAgeinMonths());
+            Console.WriteLine("oseba polnoletna : "+o1.IsAdult(o1));
+        }
+    }
+}
